2ec74ed0460609042eae7c8b6b8cf946c4bd467e
blueSwordfish <43827505+blueSwordfish@users.noreply.github.com>
2019-01-17 16:11:52 -0500

    Add files via upload

---
+++ ProcessExcelFile.py
@@ -0,0 +1,41 @@
+
+import pandas as pd
+import numpy as np
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import widgetbox
+from bokeh.models.widgets import Select
+from bokeh.layouts import row
+
+binCount = 20
+intputFile = "../data/test.xlsx"
+output_file('output_file_test.html',title='Total Revenue Figure')
+revenueColumn = 'total_revenue'
+
+df = pd.read_excel(intputFile,sheet_name="Sheet1")
+dfRevenue = df[revenueColumn]
+
+def make_plot(title, hist, edges, x, y):
+    fig = figure(title=title, tools='', background_fill_color='white')
+    fig.y_range.start = 0
+    fig.xaxis.axis_label = x
+    fig.yaxis.axis_label = y
+    fig.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+         fill_color="#036564", line_color="#033649")
+    return fig
+
+
+hist,edges = np.histogram(dfRevenue.values, bins=binCount)
+plot = make_plot("Total Revenue in $", hist, edges, "Revenue", "Organizations")
+
+
+callback = CustomJS(args=dict(source=histData), code="""
+    binCount=cb_obj.value;
+    hist,edges = np.histogram(dfRevenue.values, bins=binCount)
+    plot = make_plot("Total Revenue in $", hist, edges, "Revenue", "Organizations")
+""")
+
+select = Select(title="Bin Count:", value="20", options=["20", "40","60", "80", "100", "120"])
+select.on_change('value',callback) 
+
+layout=row(plot, widgetbox(select))
+show(layout)
